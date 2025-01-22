84815bc2db62e52cf2deed5b98cd35e5c1989c47
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2025-01-22 17:43:54 -0500

    generando vista sencilla factura

--- src/app/components/invoice/invoice.component.ts
+++ src/app/components/invoice/invoice.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Invoice } from 'src/app/models/invoice';
+import { InvoiceService } from 'src/app/services/invoice.service';
 
 @Component({
   selector: 'app-invoice',
   templateUrl: './invoice.component.html'
 })
-export class InvoiceComponent {
+export class InvoiceComponent implements OnInit{
+
+  invoice!: Invoice;
+
+  constructor( private service: InvoiceService){}
+
+  ngOnInit(): void {
+
+    this.invoice = this.service.getInvoice();
+    
+  }
 
 }
